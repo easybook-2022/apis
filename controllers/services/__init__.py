0d0e7124bf061d9061828339cc9ad260fbbc3f20
Robogram <kmrobogram@gmail.com>
2021-06-24 17:44:02 -0400

    user pages basic apis setup

--- controllers/services/__init__.py
+++ controllers/services/__init__.py
@@ -11,4 +11,28 @@ def welcome():
 def request_appointment():
 	serviceid = "29c9d9fsdkjfslkf-sdjfldsjf"
 
-	return jsonify({ "serviceid": serviceid })
+	return jsonify({ "serviceid": serviceid, "action": "request appointment" })
+
+@app.route("/cancel_purchase", methods=["POST"])
+def cancel_purchase():
+	orderid = "dsjfkldsjdsfsd9fsdjfkdsjf"
+
+	return jsonify({ "orderid": orderid, "action": "cancel purchase" })
+
+@app.route("/confirm_purchase", methods=["POST"])
+def confirm_purchase():
+	orderid = "sdjfklsdsdsdfidsfsddkjf"
+
+	return jsonify({ "orderid": orderid, "action": "confirm purchase" })
+
+@app.route("/cancel_request", methods=["POST"])
+def cancel_request():
+	serviceid = "29c9d9fsdkjfslkf-sdjfldsjf"
+
+	return jsonify({ "serviceid": serviceid, "action": "cancel request" })
+
+@app.route("/confirm_request", methods=["POST"])
+def confirm_request():
+	serviceid = "29c9d9fsdkjfslkf-sdjfldsjf"
+
+	return jsonify({ "serviceid": serviceid, "action": "confirm request" })
